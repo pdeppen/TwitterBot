39ca778580ad36cc837545484eeaf330eef08c3c
pdeppen <pdeppen17@aol.com>
2018-12-13 19:05:53 -0500

    added morgan package

--- server/bot.js
+++ server/bot.js
@@ -14,11 +14,14 @@ var T = new Twit(config)
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(morgan('combined'))
+
 const port = 3001
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
